58af242793cfc66b796512631f6879d7a341a52e
kaitowatanabe <kalbus1892@gmail.com>
2017-04-05 00:24:44 +0900

    Add xcode project

---
+++ saku/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  saku
+//
+//  Created by kaito on 2017/04/05.
+//  Copyright © 2017年 kaito. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ saku/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  saku
+//
+//  Created by kaito on 2017/04/05.
+//  Copyright © 2017年 kaito. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
